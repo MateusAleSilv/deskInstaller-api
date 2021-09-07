ddbf4e2dd90e567b0949a0cd9ba7be9d92ec09aa
MateusSK <mateuskeu22@gmail.com>
2021-09-06 21:35:19 -0300

    Commit

---
+++ ondatermica-api/src/main/java/com/ondatermica/OndatermicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ondatermica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndatermicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndatermicaApiApplication.class, args);
+	}
+
+}
---
+++ ondatermica-api/src/test/java/com/ondatermica/OndatermicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondatermica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndatermicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
