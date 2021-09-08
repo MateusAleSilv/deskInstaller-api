9e91e05c61fb0803a586aa3e5fe6f2d59ad312f5
MateusSK <mateuskeu22@gmail.com>
2021-09-08 13:33:50 -0300

    up

--- ondatermica/src/main/java/br/com/ondatermica/ondatermica/controller/clienteController.java
+++ ondatermica/src/main/java/br/com/ondatermica/ondatermica/controller/clienteController.java
@@ -33,8 +33,8 @@ public class clienteController {
 	
 	@PutMapping("/{put}")
 	public String atualizar(@PathVariable String nNome) {
-		this.nome = nNome;
-			return "Nome atualizado com sucesso";
+		this.salvar(nNome);
+		return "Nome atualizado com sucesso";
 	}
 	
 	@DeleteMapping("/{del}")
